d7dcba97dbe918feb929462fcdee06d3491b20d1
yogendra singh <yogendra@i2k2.com>
2021-07-24 12:39:41 +0530

    theme added

--- src/app/modules/auth/register/register-routing.module.ts
+++ src/app/modules/auth/register/register-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register.component';
 
 const routes: Routes = [
   {
     path:'',
-    loadChildren:()=> import('./modules/auth/auth.module').then(m=>m.AuthModule)
+    component:RegisterComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class RegisterRoutingModule { }
